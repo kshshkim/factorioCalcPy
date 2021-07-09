8f5296bf271d34844d8432ee8bd190fe01845aad
Seonghyeon Kim <kshshkim@gmail.com>
2021-07-09 17:21:20 +0900

    category 정보 추가

--- data/production_machine_dict.py
+++ data/production_machine_dict.py
@@ -3,71 +3,78 @@ production_machine_dict = {
         "name": "assembling-machine-1",
         "production_rate": 0.5,
         "module_slots": 0,
-        "power_consumption": 75
+        "power_consumption": 75,
+        "category": "crafting"
     },
     "assembling-machine-2": {
         "name": "assembling-machine-2",
         "production_rate": 0.75,
         "module_slots": 2,
-        "power_consumption": 150
+        "power_consumption": 150,
+        "category": "advanced-crafting"
     },
     "assembling-machine-3": {
         "name": "assembling-machine-3",
         "production_rate": 1.25,
         "module_slots": 4,
-        "power_consumption": 375
+        "power_consumption": 375,
+        "category": "advanced-crafting"
+
     },
     "stone-furnace": {
         "name": "stone-furnace",
         "production_rate": 1,
         "module_slots": 0,
         "power_consumption": 90,
-        "group": "furnace"
+        "group": "furnace",
+        "category": "smelting"
     },
     "steel-furnace": {
         "name": "steel-furnace",
         "production_rate": 2,
         "module_slots": 0,
         "power_consumption": 90,
-        "group": "furnace"
+        "group": "furnace",
+        "category": "smelting"
     },
     "electric-furnace": {
         "name": "electric-furnace",
         "production_rate": 2,
         "module_slots": 2,
         "power_consumption": 180,
-        "group": "furnace"
+        "group": "furnace",
+        "category": "smelting"
     },
     "burner-mining-drill": {
         "name": "burner-mining-drill",
         "production_rate": 0.25,
         "module_slots": 0,
-        "group": "mining-drill"
+        "category": "mining-drill"
     },
     "electric-mining-drill": {
         "name": "electric-mining-drill",
         "production_rate": 0.5,
         "module_slots": 0,
-        "group": "mining-drill"
+        "category": "mining-drill"
     },
     "offshore-pump": {
-        "name": "offshore-pump"
-
-    },
-    "lab": {
-        "name": "lab"
+        "name": "offshore-pump",
+        "category": "water"
     },
     "pumpjack": {
         "name": "pumpjack",
-        "group": "crue-oil"
+        "category": "crude-oil"
     },
     "oil-refinery": {
-        "name": "oil-refinery"
+        "name": "oil-refinery",
+        "category":"oil-processing"
     },
     "chemical-plant": {
-        "name": "chemical-plant"
+        "name": "chemical-plant",
+        "category": "chemistry"
     },
     "centrifuge": {
-        "name": "centrifuge"
+        "name": "centrifuge",
+        "category": "centrifuging"
     }
 }
