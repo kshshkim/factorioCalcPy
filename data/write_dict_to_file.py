c8cb5332dd84647e15fc26f9084b821f066e64a3
Seonghyeon Kim <kshshkim@gmail.com>
2021-07-09 17:21:51 +0900

    큰따옴표를 작은따옴표로 변경

--- data/write_dict_to_file.py
+++ data/write_dict_to_file.py
@@ -6,7 +6,7 @@ class WriteDictToFile:
     def write(self, dict_obj: dict, dict_name: str, directory: str):
         towrite = json.dumps(dict_obj, sort_keys=False, indent=4)
         towrite = dict_name + ' = ' + towrite + '\n'
-        towrite = towrite.replace('true', 'True').replace('false', 'False').replace('null', 'None')
+        towrite = towrite.replace('true', 'True').replace('false', 'False').replace('null', 'None').replace('"',"'")
         with open(directory, 'w') as outf:
             outf.write(towrite)
         print(dict_name + ' converted to ' + directory)
