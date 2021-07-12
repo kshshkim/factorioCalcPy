34eea271a56ce9532a4f7c54a75c84ecf3b2be51
Seonghyeon Kim <kshshkim@gmail.com>
2021-07-12 16:26:49 +0900

    .

--- factorio_production_machine_class.py
+++ factorio_production_machine_class.py
@@ -4,7 +4,7 @@ from data.module_modifier_dict import module_modifier_dict
 
 
 class FactorioMachine:
-    # FactorioCalc 에 전달할 것
+    # FactorioProductionBlock 에 전달할 것
     total_modifier: list
     production_speed_rate: float
     power_consumption_rate: float
@@ -13,12 +13,13 @@ class FactorioMachine:
     total_requirement_modifier: float
 
     def __init__(self, factory_name: str, to_add_module_list=None, mining_research_modifier=0):
-        # FactorioCalc 에서 전달받을 것
+        # FactorioProductionBlock 에서 전달받을 것
         self.production_machine_name = factory_name
         if to_add_module_list is None:
             to_add_module_list = []
         self.added_module_list = to_add_module_list
         self.mining_research_modifier = mining_research_modifier
+        self.resource_consumption_rate = 1
 
         # production_machine_dict 에서 가져올것
         self.base_production_speed_rate = production_machine_dict[self.production_machine_name]['production_rate']
@@ -44,8 +45,7 @@ class FactorioMachine:
 
         if self.production_machine_name in pmc_list_dict['mining-drill'] or self.production_machine_name in \
                 pmc_list_dict['crude-oil']:
-            self.total_modifier[2] = self.total_modifier[2] + production_machine_dict[
-                self.production_machine_name] * self.mining_research_modifier
+            self.total_modifier[2] = self.total_modifier[2] + self.mining_research_modifier
 
         self.production_speed_rate = self.base_production_speed_rate + self.base_production_speed_rate * self.total_modifier[0]
         self.power_consumption_rate = self.base_power_consumption_rate + self.base_power_consumption_rate * self.total_modifier[1]
@@ -53,8 +53,7 @@ class FactorioMachine:
         # 추가 생산품을 생산 속도에 반영 생산속도 배수 곱연산 적용
         self.production_speed_rate = self.production_speed_rate * (1 + self.extra_product_rate)
         self.resource_consumption_rate = 1
+
         if self.extra_product_rate != 0:
             self.resource_consumption_rate = (1/self.extra_product_rate) / (1/self.extra_product_rate + 1)
 
-    def get_resource_consumption_rate(self):
-        return self.resource_consumption_rate
