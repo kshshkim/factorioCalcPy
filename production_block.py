bf100871b4fc8c796b7e00712d89e9aab8102c70
Seonghyeon Kim <kshshkim@gmail.com>
2021-07-15 17:56:32 +0900

    재작성

--- production_block.py
+++ production_block.py
@@ -4,39 +4,45 @@ from data.module_modifier_dict import module_modifier_dict
 from production_machine import ProductionMachine
 from recipe_class import RecipeClass
 
+
 class ProductionBlock:
-    def __init__(self, recipe_name, machine_name=None, module_list=None, mining_research_modifier_dict=None):
+    def __init__(self, recipe_name, machine_name=None, module_list=None, mining_research_modifier=None):
         self.recipe_name = recipe_name
         self.recipe_obj = RecipeClass(self.recipe_name)
         if module_list is None:
             module_list = []
-        if mining_research_modifier_dict is None:
-            mining_research_modifier_dict = {}
+        if mining_research_modifier is None:
+            mining_research_modifier = 0
+        self.mining_research_modifier = mining_research_modifier
         self.module_list = module_list
         self.machine_name = machine_name
         self.machine_obj = None
         self.available_machine_list = self.get_available_machine_list()
         self.update_machine()
+        self.total_modifier = self.get_total_modifier()
+
 
     def get_available_machine_list(self):
         recipe_category = self.recipe_obj.category
         machine_list = production_machine_category_list_dict[recipe_category]
         return machine_list
 
-    def update_machine(self, machine_name=None):
-        self.machine_name = machine_name
+    def update_machine(self):
         if (self.machine_name is None) or (self.machine_name not in self.available_machine_list):
             print(self.available_machine_list)
             self.machine_obj = ProductionMachine(self.available_machine_list[-1])
         else:
             self.machine_obj = ProductionMachine(self.machine_name)
+        self.set_module(self.module_list)
 
-    def set_module(self, module_code_or_list):
+    def set_module(self, module_code_or_list=None):
         check_list = None
         if type(module_code_or_list) is str:
             check_list = [module_code_or_list]
         elif type(module_code_or_list) is list:
             check_list = module_code_or_list
+        elif module_code_or_list is None:
+            check_list = []
 
         can_add = []
 
@@ -51,7 +57,7 @@ class ProductionBlock:
             else:
                 self.module_list = can_add[:self.machine_obj.module_slots]
 
-        # TODO need to refresh 기능
+        self.get_total_modifier()
         # self.module_list
 
     def can_this_module_be_added(self, module_code: str):
@@ -80,5 +86,21 @@ class ProductionBlock:
             for module in self.module_list:
                 for i in range(3):
                     total_modifier[i] += module_modifier_dict[module][i]
-                    total_modifier[i] = round(total_modifier[i], 4)
+            for i in range(3):
+                total_modifier[i] = round(total_modifier[i], 4)
+        if self.mining_research_modifier != 0 and self.recipe_obj.category == 'mining-drill':
+            total_modifier[2] += self.mining_research_modifier
+        self.total_modifier = total_modifier
+        self.get_modifiers_calculated()
         return total_modifier
+
+    def get_modifiers_calculated(self):
+        base_list = [self.machine_obj.base_speed_rate, self.machine_obj.base_power_consumption, 0]
+        return_list = [0, 0, 0]
+        for i in range(2):
+            return_list[i] = base_list[i] + base_list[i] * self.total_modifier[i]
+            return_list[2] = self.total_modifier[2]
+
+            if return_list[2] != 0:
+                return_list[0] = return_list[0] * (return_list[2] + 1)
+
--- test.py
+++ test.py
@@ -1,8 +1,25 @@
-from dependency_tree import RecipeDependency
-from oil_processing import FactorioOilNeedToRecipe
+from dependency_tracker import DependencyDictMerged
 
-asdf = RecipeDependency(recipe_name='processing-unit')
+import pprint
+from production_block import ProductionBlock
 
-print(asdf.get_total_recipe_req_dict())
-print(asdf.total_needed_item())
-
+asdf = DependencyDictMerged(recipe_name='processing-unit')
+aaaa = ProductionBlock(recipe_name='processing-unit')
+# # print(asdf.get_total_recipe_req_dict())
+# pprint.pprint(asdf.total_needed_item())
+# pprint.pp(asdf.merged_oil_dict)
+# pprint.pp(asdf.merged_recipe_dict)
+# pprint.pp(asdf.merged_item_dict)
+aaaa.set_module(['s1', 's2', 'p3', 'p2'])
+# aaaa.get_total_modifier()
+print(aaaa.module_list)
+aaaa.update_machine('assembling-machine-2')
+print(aaaa.module_list)
+pprint.pprint(aaaa.total_modifier)
+#
+# pprint.pp('eaeas')
+#
+# pprint.pp(asdf.merged_item_dict)
+# pprint.pp(asdf.merged_recipe_dict)
+# pprint.pp(asdf.merged_gen_dict)
+# pprint.pp(asdf.merged_oil_dict)
