d54c8812e14c4342c90e04d12971daa4db1d4592
Seonghyeon Kim <kshshkim@gmail.com>
2021-07-15 14:22:25 +0900

    refactored

---
+++ production_machine.py
@@ -0,0 +1,12 @@
+from data.production_machine_dict import production_machine_dict
+from data.module_modifier_dict import module_modifier_dict
+from data.productivity_module_available_list import productivity_module_available_list
+
+
+class ProductionMachine:
+    def __init__(self, machine_name: str):
+        self.machine_name = machine_name
+        self.production_rate = production_machine_dict[self.machine_name]['production_rate']
+        self.module_slots = production_machine_dict[self.machine_name]['module_slots']
+        self.power_consumption = production_machine_dict[self.machine_name]['power_consumption']
+        self.category = production_machine_dict[self.machine_name]['category']
