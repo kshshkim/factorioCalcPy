228b4be8e2d275b150ee4776892b529b526434fc
Seonghyeon Kim <kshshkim@gmail.com>
2021-07-23 10:17:04 +0900

    블루프린트 삭제

--- FactorioCalcBase/test.py
+++ FactorioCalcBase/test.py
@@ -1,25 +1,23 @@
-# from FactorioCalcBase.calculator_base import FactorioCalculatorBase
-# import sys
+from FactorioCalcBase.calculator_base import FactorioCalculatorBase
+
+
+ddd = FactorioCalculatorBase('processing-unit')
+ddd.change_machine_to_specific_block('advanced-circuit', 'assembling-machine-2')
+print(ddd.json_out())
+# import random
+# import time
 #
-# print(sys.path)
+# class SangTae:
+#     def __init__(self):
+#         self.asdf = [1,2,3,4,5]
 #
-# ddd = FactorioCalculatorBase('processing-unit')
-# ddd.change_machine_to_specific_block('advanced-circuit', 'assembling-machine-2')
-# print(ddd.json_out())
-import random
-import time
-
-class SangTae:
-    def __init__(self):
-        self.asdf = [1,2,3,4,5]
-
-
-asdd = SangTae()
-
-it = iter(asdd.asdf)
-
-print(next(it))
-print(next(it))
-print(next(it))
-print(next(it))
-time.sleep(10)
+#
+# asdd = SangTae()
+#
+# it = iter(asdd.asdf)
+#
+# print(next(it))
+# print(next(it))
+# print(next(it))
+# print(next(it))
+# time.sleep(10)
