4528dbd8872c524074c7cfbfdd62b75fcbf0a473
Seonghyeon Kim <kshshkim@gmail.com>
2021-07-23 10:00:33 +0900

    블루프린트 삭제

--- FactorioCalcWeb/__init__.py
+++ FactorioCalcWeb/__init__.py
@@ -1,12 +1,50 @@
-from flask import Flask
+from flask import Flask, render_template, request, current_app, jsonify
 from FactorioCalcWeb.session import SessionManagerClass
 
+app = Flask(__name__)
+app.secret_key = 'Factorio'
 
-def create_app():
-    app = Flask(__name__)
-    app.secret_key = 'Factorio'
-    from .views import main_views
-    app.register_blueprint(main_views.bp)
-    app.config['JSON_SORT_KEYS'] = False
-    app.config.factorio_session_manager = SessionManagerClass()
-    return app
+app.config['JSON_SORT_KEYS'] = False
+app.config.factorio_session_manager = SessionManagerClass()
+
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+
+@app.route('/session', methods=['POST'])
+def initial_session_create():
+    print(request.get_json())
+    rand_id = request.get_json()['rand_id']
+    print(rand_id)
+    sm_instance: SessionManagerClass = current_app.config.factorio_session_manager
+    if rand_id not in sm_instance.SD.keys():
+        sm_instance.add_session(rand_id=rand_id)
+    return 'session_set'
+
+
+@app.route('/session/imalive', methods=['POST'])
+def session_keep_alive():
+    print(request.get_json())
+    rand_id = request.get_json()['rand_id']
+
+    sm_instance: SessionManagerClass = current_app.config.factorio_session_manager
+    if sm_instance.SD.get(rand_id) is not None:
+        sm_instance.im_alive(rand_id=rand_id)
+        return 'ok'
+    else:
+        return 'not_ok'
+
+
+@app.route('/api', methods=['POST'])
+def calc_controller():
+    to_toss = request.get_json()
+    rand_id = to_toss['rand_id']
+    sm_instance: SessionManagerClass = current_app.config.factorio_session_manager
+    refreshed_info = sm_instance.SD[rand_id].calc_instance.handle_request(to_toss)
+    return jsonify(refreshed_info)
+
+
+if __name__ == "__main__":
+     app.run()
--- FactorioCalcWeb/views/main_views.py
+++ FactorioCalcWeb/views/main_views.py
@@ -1,42 +1,42 @@
-from flask import Blueprint, render_template, request, current_app, jsonify
-from FactorioCalcWeb.session import SessionManagerClass
-
-bp = Blueprint('main', __name__, url_prefix='/')
-
-
-@bp.route('/')
-def main():
-    return render_template('index.html')
-
-
-@bp.route('/session', methods=['POST'])
-def initial_session_create():
-    print(request.get_json())
-    rand_id = request.get_json()['rand_id']
-    print(rand_id)
-    sm_instance: SessionManagerClass = current_app.config.factorio_session_manager
-    if rand_id not in sm_instance.SD.keys():
-        sm_instance.add_session(rand_id=rand_id)
-    return 'session_set'
-
-
-@bp.route('/session/imalive', methods=['POST'])
-def session_keep_alive():
-    print(request.get_json())
-    rand_id = request.get_json()['rand_id']
-
-    sm_instance: SessionManagerClass = current_app.config.factorio_session_manager
-    if sm_instance.SD.get(rand_id) is not None:
-        sm_instance.im_alive(rand_id=rand_id)
-        return 'ok'
-    else:
-        return 'not_ok'
-
-
-@bp.route('/api', methods=['POST'])
-def calc_controller():
-    to_toss = request.get_json()
-    rand_id = to_toss['rand_id']
-    sm_instance: SessionManagerClass = current_app.config.factorio_session_manager
-    refreshed_info = sm_instance.SD[rand_id].calc_instance.handle_request(to_toss)
-    return jsonify(refreshed_info)
+# from flask import Blueprint, render_template, request, current_app, jsonify
+# from FactorioCalcWeb.session import SessionManagerClass
+#
+# bp = Blueprint('main', __name__, url_prefix='/')
+#
+#
+# @bp.route('/')
+# def main():
+#     return render_template('index.html')
+#
+#
+# @bp.route('/session', methods=['POST'])
+# def initial_session_create():
+#     print(request.get_json())
+#     rand_id = request.get_json()['rand_id']
+#     print(rand_id)
+#     sm_instance: SessionManagerClass = current_app.config.factorio_session_manager
+#     if rand_id not in sm_instance.SD.keys():
+#         sm_instance.add_session(rand_id=rand_id)
+#     return 'session_set'
+#
+#
+# @bp.route('/session/imalive', methods=['POST'])
+# def session_keep_alive():
+#     print(request.get_json())
+#     rand_id = request.get_json()['rand_id']
+#
+#     sm_instance: SessionManagerClass = current_app.config.factorio_session_manager
+#     if sm_instance.SD.get(rand_id) is not None:
+#         sm_instance.im_alive(rand_id=rand_id)
+#         return 'ok'
+#     else:
+#         return 'not_ok'
+#
+#
+# @bp.route('/api', methods=['POST'])
+# def calc_controller():
+#     to_toss = request.get_json()
+#     rand_id = to_toss['rand_id']
+#     sm_instance: SessionManagerClass = current_app.config.factorio_session_manager
+#     refreshed_info = sm_instance.SD[rand_id].calc_instance.handle_request(to_toss)
+#     return jsonify(refreshed_info)
